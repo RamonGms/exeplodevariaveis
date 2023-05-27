e015d2c5ab53c6d38867fc58ffdecfab2b0f9662
RamonGms <106332513+RamonGms@users.noreply.github.com>
2023-05-26 22:48:39 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+//let nome = "Ramon";
+//let idade = 32;
+//idade = "Abc";
+//console.log(nome)
+//console.log(idade)
+
+const endereço = "Avenida 1";
+
+let preçoDoLeite = 9.5;
+let preçoDoQueijo = 30;
+let preçoDaPizza;
+let preçoDoPão;
+
+preçoDoPão = 1,50;
+console.log ("Preço do leite:", preçoDoLeite);
+console.log ("Preço do queijo:", preçoDoQueijo);
+console.log ("Preço da pizza:", preçoDaPizza);
+console.log ("Preço do pão:", preçoDoPão);
+preçoDoLeite = 8
+
+console.log ("Preço do leite:", preçoDoLeite);
+console.log ("Preço do queijo:", preçoDoQueijo);
